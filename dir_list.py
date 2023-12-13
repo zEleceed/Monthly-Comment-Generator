e86b86b0ce830e251dcf628160c317c583f8b6f6
zEleceed <133176949+zEleceed@users.noreply.github.com>
2023-12-14 08:46:45 +0900

    Changed for code to be more readable

--- dir_list.py
+++ dir_list.py
@@ -8,13 +8,9 @@ def dir_list(givenDirectory):
     real_list = []
     list_of_folders = Path(givenDirectory).iterdir()
     for folder in list_of_folders:
-        if folder.is_dir() and (folder.name[0] == "."):
-            continue
-        elif folder.name == "venv":
-            continue
-        elif folder.name == "__pycache__":
-            continue
-        elif folder.is_dir():
+        if folder.is_dir() and not folder.name.startswith('.') and folder.name not in ["venv", "__pycache__"]:
             real_list.append(folder)
     return sorted(real_list)
 
+
+print(dir_list(rawr))
